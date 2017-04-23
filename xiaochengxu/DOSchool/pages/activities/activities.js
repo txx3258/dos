19ccbba1a923bb4c846d7da42b89cb276521a68a
浪匿泉石 <yuquanwang1990@163.com>
2017-04-23 23:32:55 +0800

    dos小程序活动报名页面布局

---
+++ xiaochengxu/DOSchool/pages/activities/activities.js
@@ -0,0 +1,26 @@
+// pages/events/events.js
+var app = getApp();
+Page({
+  data: {},
+
+  enterRegister: function () {
+    wx.navigateTo({
+      url: '../activities/activitiesRegister/activitiesRegister'
+    })
+  },
+  onLoad: function (options) {
+    // 页面初始化 options为页面跳转所带来的参数
+  },
+  onReady: function () {
+    // 页面渲染完成
+  },
+  onShow: function () {
+    // 页面显示
+  },
+  onHide: function () {
+    // 页面隐藏
+  },
+  onUnload: function () {
+    // 页面关闭
+  }
+})
