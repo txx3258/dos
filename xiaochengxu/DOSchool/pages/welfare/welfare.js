08840d6aa5193dea4bf20cd31e226ac6774bd1ae
浪匿泉石 <yuquanwang1990@163.com>
2017-05-10 17:58:24 +0800

    DOS小程序福利页布局
    对于button的布局使用绝对定位，其位置由正弦函数得到

--- xiaochengxu/DOSchool/pages/welfare/welfare.js
+++ xiaochengxu/DOSchool/pages/welfare/welfare.js
@@ -1,14 +1,44 @@
-//logs.js
+//welfare.js
 var util = require('../../utils/util.js')
 Page({
   data: {
-    logs: []
+    urlList: {
+      "sunImage": "https://modao.cc/uploads2/images/738/7383133/raw_1485002896.png",
+      "runImage": "https://modao.cc/uploads2/images/738/7383239/raw_1485004176.png",
+      "rightImage": "/images/welfare/arrowRight.png",
+      "homeImage": "/images/welfare/home.png",
+    },
+    welfareList: [
+      { "name": "DOS红包节", "color": "#E84E40" }, { "name": "DOS图书馆", "color": "#FFA726" }, { "name": "DOS缘梦园", "color": "#F4FC00" }, { "name": "爱奇艺VIP", "color": "#2BAF2B" }, { "name": "DOS月分红", "color": "#738FFE" }, { "name": "DOS圆梦帮", "color": "#78909C" }, { "name": "DOS年终奖", "color": "#AB47BC" }
+    ],
+    buttonPosition:{
+      "toTop": [],
+      "toLeft": []
+    }
+
   },
+
+  returnHomePage: function () {
+    console.log("点击");
+    wx.switchTab({
+      url: "../activities/activities"
+    });
+  },
+
+  getButtonPosition: function() {
+    var length = this.data.welfareList.length;
+    console.log(length);
+    var toTop = [], toLeft = [];
+    for(let i = 0; i < length; i++) {
+      toTop[i] = 1000.0 / length * (i + 0.5);
+      let a = -200.0, w = 2 * 3.14159 / 720 , x = toTop[i];
+      toLeft[i] = a * Math.cos(w * x) + 200.0;
+    }
+    this.setData({buttonPosition: {toTop, toLeft}});
+  },
+
   onLoad: function () {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(function (log) {
-        return util.formatTime(new Date(log))
-      })
-    })
+    console.log("onload");
+    this.getButtonPosition();
   }
 })
